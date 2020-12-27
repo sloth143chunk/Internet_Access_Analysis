b90c38ea521237cad386dda0fd6893d15ecc8c6f
BeccaAdventuresofbntTC <adventuresofbnt@Beccas-MacBook-Pro.local>
2020-12-26 20:58:50 -0800

    state and county boundries added

---
+++ Static/js/config.js
@@ -0,0 +1,2 @@
+// Public MapBox Key
+const API_KEY = 'pk.eyJ1IjoidGNyb3R1bmRhIiwiYSI6ImNrZnl3N2YzZTBnZWwycW9kZDBka3Y0NDQifQ.ThAXAx5xGdYCO4CgnWv9KQ';
--- Static/js/map.js
+++ Static/js/map.js
@@ -2,23 +2,75 @@
 console.log("working");
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	accessToken: API_KEY
 });
 
+let outdoors = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
 let map = L.map('mapid', {
 	center: [40.7, -94.5],
 	zoom: 3,
-	layers: [streets]
+    layers: [light]
 });
-
-streets.addTo(map)
 
 // Create a base layer that holds all three maps.
 let baseMaps = {
-    "Streets": streets,
-    //"Satellite": satelliteStreets,
-    //"Dark": dark
+    "Light Map": light,
+    "Outdoor Map": outdoors,
 };
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+//https://github.com/jgoodall/us-maps
+let usCounties = "https://raw.githubusercontent.com/jgoodall/us-maps/master/geojson/county.geo.json"
+let usStates = "https://raw.githubusercontent.com/jgoodall/us-maps/master/geojson/state.geo.json"
+
+// Create a style for county lines.
+let countyStyle = {
+    color: '#003366',
+    fillColor: '#ffff00',
+    weight: .5,
+}
+
+// Create a style for state lines.
+let stateStyle = {
+    color: '#5f0087',
+    //fillColor: '#ffff00',
+    weight: 1,
+}
+
+// Grabbing States
+d3.json(usStates).then(function(data) {
+    // printing
+    console.log(data);
+
+    // adding styling
+    L.geoJson(data,{
+        style:stateStyle
+    })
+    .addTo(map);
+});
+
+// Grabbing Counties
+d3.json(usCounties).then(function(data) {
+    // printing
+    console.log(data);
+
+    // adding styling
+    L.geoJson(data,{
+        style:countyStyle,
+        onEachFeature: function onEachFeature(feature, layer) {
+            layer.bindPopup("<h4>"+feature.properties.NAMELSAD10+", "+feature.properties.STUSPS10+"</h4>"+
+            "<p>"+"GEO ID: "+feature.properties.GEOID10+"</p>"+
+            "<p>"+"Internet Score: "+"</p>")
+    }})
+    .addTo(map);
+});
